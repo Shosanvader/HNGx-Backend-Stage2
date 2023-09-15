646b67652e7808a42984b5802923b823e2608486
Shosan Vader <okediadip@gmail.com>
2023-09-15 23:28:13 +0100

    Edited database

--- api/models.py
+++ api/models.py
@@ -1,4 +1,8 @@
 from django.db import models
 
 class Person(models.Model):
+    user_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return  self.name
--- hngproject/settings.py
+++ hngproject/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -69,15 +69,12 @@ WSGI_APPLICATION = 'hngproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'), 
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
