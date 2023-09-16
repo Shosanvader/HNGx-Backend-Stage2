e178b27ff079f09e9d38e4e8a086d50e78fbf819
Shosan Vader <okediadip@gmail.com>
2023-09-16 14:13:15 +0100

    Anyhow

--- api/views.py
+++ api/views.py
@@ -1,7 +1,32 @@
-from rest_framework import viewsets  
-from .serializers import PersonSerializer  
-from .models import Person  
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-class PersonView(viewsets.ModelViewSet):  
-    serializer_class = PersonSerializer  
+from .serializers import PersonSerializer
+from .models import Person
+
+class PersonView(viewsets.ModelViewSet):
+    serializer_class = PersonSerializer
     queryset = Person.objects.all()
+
+    # Custom create method
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
+
+    # Custom update method
+    def update(self, request, *args, **kwargs):
+        # Your custom logic here
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+    # Custom destroy (delete) method
+    def destroy(self, request, *args, **kwargs):
+        # Your custom logic here
+        instance = self.get_object()
+        instance.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- hngproject/urls.py
+++ hngproject/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 urlpatterns = [
-    re_path(r'^api/?', include('api.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # Include the API app's URLs
 ]
